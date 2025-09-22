b9fa7684d0230e5e75ec0a8cca3b1840f5f6b003
Adrian Schyja <adrian.schyja@googlemail.com>
2025-09-22 20:37:54 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+![Logo](docs/rodret_128.png)
+
+# ioBroker.ikea-rodret
+
+[![NPM version](https://img.shields.io/npm/v/iobroker.rodret.svg)](https://www.npmjs.com/package/iobroker.rodret)
+[![Downloads](https://img.shields.io/npm/dm/iobroker.rodret.svg)](https://www.npmjs.com/package/iobroker.rodret)
+![Number of Installations](https://iobroker.live/badges/rodret-installed.svg)
+![Current version in stable repository](https://iobroker.live/badges/rodret-stable.svg)
+
+[![NPM](https://nodei.co/npm/iobroker.rodret.png?downloads=true)](https://nodei.co/npm/iobroker.rodret/)
+
+**Tests:** ![Test and Release](https://github.com/asc-ii/ioBroker.rodret/workflows/Test%20and%20Release/badge.svg)
+
+## IKEA RODRET Dimmer (E2201) adapter for ioBroker
+
+This adapter lets you use the IKEA RODRET wireless dimmer as a true dimmer for a selected light in ioBroker.
+Normally, the RODRET only sends a single “action” event (e.g. `brightness_move_up`) when the button is pressed,
+so you would have to create your own JavaScript to achieve continuous dimming.
+
+With this adapter you simply configure a target light, and the adapter will
+brighten or dim it smoothly while you hold the button — no extra scripting required.
+
+## Changelog
+
+<!--
+	Placeholder for the next version (at the beginning of the line):
+	### **WORK IN PROGRESS**
+-->
+
+### **WORK IN PROGRESS**
+
+- (Adrian Schyja) initial release
+
+## License
+
+MIT License
+
+Copyright (c) 2025 Adrian Schyja <adrian.schyja@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ main.js
@@ -0,0 +1,280 @@
+'use strict';
+
+/*
+ * Created with @iobroker/create-adapter v2.6.5
+ */
+
+const utils = require('@iobroker/adapter-core');
+const ACTION_SUFFIX = 'action';
+
+class IkeaRodret extends utils.Adapter {
+	/**
+	 * @param {Partial<utils.AdapterOptions>} [options={}]
+	 */
+	constructor(options) {
+		super({
+			...options,
+			name: 'ikea-rodret',
+		});
+		this.on('ready', this.onReady.bind(this));
+		this.on('stateChange', this.onStateChange.bind(this));
+		this.on('objectChange', this.onObjectChange.bind(this));
+		this.on('unload', this.onUnload.bind(this));
+
+		this.dimInterval = null;
+	}
+
+	/**
+	 * Is called when databases are connected and adapter received configuration.
+	 */
+	async onReady() {
+		this.log.debug('onReady');
+
+		await this.createDebugDataPoints();
+
+		if (!this.config.rodretId) {
+			this.log.error(`Device not configured - please check instance configuration of ${this.namespace}`);
+			return;
+		}
+		if (!this.config.lightId) {
+			this.log.error(`Light not configured - please check instance configuration of ${this.namespace}`);
+			return;
+		}
+		if (!this.config.brightnessId) {
+			this.log.error(
+				`Brightness/Dimmer not configured - please check instance configuration of ${this.namespace}`,
+			);
+			return;
+		}
+
+		this.log.debug('RODRET device id: ' + this.config.rodretId);
+		this.log.debug('RODRET light id: ' + this.config.lightId);
+		this.log.debug('RODRET brightness id: ' + this.config.brightnessId);
+
+		// subscribe to button action event
+		this.log.debug(`Subscribing to ${this.rodretAction()}`);
+		await this.subscribeForeignStatesAsync(this.rodretAction());
+	}
+
+	/**
+	 * Is called when adapter shuts down - callback has to be called under any circumstances!
+	 * @param {() => void} callback
+	 */
+	onUnload(callback) {
+		this.log.debug('Unloading adapter...');
+		try {
+			if (this.dimInterval !== null) {
+				clearInterval(this.dimInterval);
+				this.dimInterval = null;
+			}
+
+			callback();
+		} catch (_e) {
+			callback();
+		}
+	}
+
+	/**
+	 * Is called if a subscribed object changes
+	 * @param {string} id
+	 * @param {ioBroker.Object | null | undefined} obj
+	 */
+	onObjectChange(id, obj) {
+		if (obj) {
+			// The object was changed
+			this.log.debug(`object ${id} changed: ${JSON.stringify(obj)}`);
+		} else {
+			// The object was deleted
+			this.log.debug(`object ${id} deleted`);
+		}
+	}
+
+	/**
+	 * Is called if a subscribed state changes
+	 * @param {string} id
+	 * @param {ioBroker.State | null | undefined} state
+	 */
+	async onStateChange(id, state) {
+		if (state) {
+			// The state was changed
+			this.log.debug(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+			if (id === this.rodretAction() && state.ack === false) {
+				await this.handleRodretAction(String(state.val));
+			}
+		} else {
+			// The state was deleted
+			this.log.debug(`state ${id} deleted`);
+		}
+	}
+
+	/**
+	 * Handles actions triggered by the RODRET device.
+	 * @param {string} action Any of the allowed RODRET action
+	 * states ("on", "off", "brightness_move_up", "brightness_move_down",
+	 * "brightness_stop").
+	 */
+	async handleRodretAction(action) {
+		this.log.debug(`Handling RODRET action: ${action}`);
+
+		if (this.dimInterval !== null) {
+			clearInterval(this.dimInterval);
+			this.dimInterval = null;
+		}
+
+		switch (action) {
+			case 'on':
+				this.log.debug(`Switching light ${this.config.lightId} on`);
+				this.switchLight(true);
+				break;
+
+			case 'off':
+				this.log.debug(`Switching light ${this.config.lightId} off`);
+				this.switchLight(false);
+				break;
+
+			case 'brightness_move_up':
+				this.log.debug('Brightening the light');
+				this.dimInterval = setInterval(
+					() => this.changeBrightness(this.config.dimStep),
+					this.config.dimInterval,
+				);
+				break;
+
+			case 'brightness_move_down':
+				this.log.debug('Dimming the light');
+				// dimInterval = setInterval(() => changeBrightness(-DIM_STEP), DIM_INTERVAL);
+				break;
+
+			case 'brightness_stop':
+				// dimInterval = null;
+				break;
+		}
+	}
+
+	/**
+	 * @returns {string} The 'action' datapoint ID of the configured RODRET device.
+	 */
+	rodretAction() {
+		return `${this.config.rodretId}.${ACTION_SUFFIX}`;
+	}
+
+	/**
+	 * @param {boolean} onOrOff
+	 */
+	async switchLight(onOrOff) {
+		if (!this.config.lightId) {
+			this.log.error('Light not configured - please check instance configuration');
+			return;
+		}
+		// switch the light on or off
+		await this.setForeignStateAsync(this.config.lightId, { val: onOrOff, ack: true });
+	}
+
+	/**
+	 * Increase/decrease brightness by delta, keeping within [0, 100].
+	 * @param {number} delta - Brightness change (+/-).
+	 */
+	async changeBrightness(delta) {
+		const current = await this.getForeignStateAsync(String(this.config.brightnessId));
+		if (!current || current.val === null) {
+			this.log.error(`Cannot change brightness - state ${this.config.brightnessId} not found or has no value`);
+			return;
+		}
+
+		const newBrightness = Math.max(0, Math.min(100, Number(current.val) + delta));
+		await this.setForeignStateAsync(String(this.config.brightnessId), { val: newBrightness, ack: true });
+
+		const isOn = newBrightness > 1;
+		await this.switchLight(isOn);
+	}
+
+	async createDebugDataPoints() {
+		await this.setForeignObjectNotExistsAsync('zigbee', {
+			type: 'folder',
+			common: {
+				name: 'zigbee',
+				role: 'folder',
+			},
+			native: {},
+		});
+
+		const adapter = this;
+
+		async function createDevice(rootId) {
+			await adapter.setForeignObjectNotExistsAsync('zigbee.' + rootId, {
+				type: 'device',
+				common: {
+					name: 'RODRET Switch',
+					role: 'device',
+				},
+				native: {},
+			});
+			await adapter.setForeignObjectNotExistsAsync('zigbee.' + rootId + '.action', {
+				type: 'state',
+				common: {
+					name: 'Triggered action (e.g. a button click)',
+					type: 'string',
+					states: {
+						on: 'on',
+						off: 'off',
+						brightness_move_up: 'brightness_move_up',
+						brightness_move_down: 'brightness_move_down',
+						brightness_stop: 'brightness_stop',
+					},
+					read: true,
+					write: true,
+					role: 'state',
+				},
+				native: {},
+			});
+		}
+
+		await createDevice('e406bffffe3651e5_1');
+		await createDevice('e406bffffe3651e5_2');
+		await createDevice('e406bffffe3651e5_3');
+
+		await this.setForeignObjectNotExistsAsync('zigbee.light_1', {
+			type: 'device',
+			common: {
+				name: 'Light 1',
+				role: 'device',
+			},
+			native: {},
+		});
+		await this.setForeignObjectNotExistsAsync('zigbee.light_1.switch', {
+			type: 'state',
+			common: {
+				name: 'switch',
+				type: 'boolean',
+				read: true,
+				write: true,
+				role: 'switch',
+			},
+			native: {},
+		});
+		await this.setForeignObjectNotExistsAsync('zigbee.light_1.brightness', {
+			type: 'state',
+			common: {
+				name: 'Brightness',
+				type: 'number',
+				read: true,
+				write: true,
+				role: 'level.dimmer',
+				min: 0,
+				max: 100,
+			},
+			native: {},
+		});
+	}
+}
+
+if (require.main !== module) {
+	// Export the constructor in compact mode
+	/**
+	 * @param {Partial<utils.AdapterOptions>} [options={}]
+	 */
+	module.exports = (options) => new IkeaRodret(options);
+} else {
+	// otherwise start the instance directly
+	new IkeaRodret();
+}
