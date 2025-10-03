71952c926bd212b4d5f443b424140a78bcc33997
Adrian Schyja <adrian.schyja@googlemail.com>
2025-10-03 23:34:55 +0200

    chore: migrate to ESLint 9 and fix related errors
    fix #5

--- main.js
+++ main.js
@@ -8,132 +8,138 @@ const utils = require('@iobroker/adapter-core');
 const RodretDevice = require('./rodretdevice');
 
 class IkeaRodret extends utils.Adapter {
-	/**
-	 * @param {Partial<utils.AdapterOptions>} [options={}]
-	 */
-	constructor(options) {
-		super({
-			...options,
-			name: 'ikea-rodret',
-		});
-		this.on('ready', this.onReady.bind(this));
-		this.on('stateChange', this.onStateChange.bind(this));
-		this.on('objectChange', this.onObjectChange.bind(this));
-		this.on('unload', this.onUnload.bind(this));
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options] ioBroker Adapter options.
+     */
+    constructor(options) {
+        super({
+            ...options,
+            name: 'ikea-rodret',
+        });
+        this.on('ready', this.onReady.bind(this));
+        this.on('stateChange', this.onStateChange.bind(this));
+        this.on('objectChange', this.onObjectChange.bind(this));
+        this.on('unload', this.onUnload.bind(this));
 
-		/** @type {RodretDevice[]} */
-		this.devices = [];
-	}
+        this.devices = [];
+    }
 
-	/**
-	 * Is called when databases are connected and adapter received configuration.
-	 */
-	async onReady() {
-		if (!Array.isArray(this.config.rodretDevices) || this.config.rodretDevices.length === 0) {
-			this.log.error(`No devices configured - please check instance configuration of ${this.namespace}`);
-			return;
-		}
+    /**
+     * Is called when databases are connected and adapter received configuration.
+     */
+    async onReady() {
+        if (!Array.isArray(this.config.rodretDevices) || this.config.rodretDevices.length === 0) {
+            this.log.error(`No devices configured - please check instance configuration of ${this.namespace}`);
+            return;
+        }
 
-		for (const devConf of this.config.rodretDevices) {
-			if (!devConf.rodretId || !devConf.lightId || !devConf.brightnessId) {
-				this.log.warn(`Skipping invalid device configuration: ${JSON.stringify(devConf)}`);
-				continue;
-			}
+        for (const devConf of this.config.rodretDevices) {
+            if (!devConf.rodretId || !devConf.lightId || !devConf.brightnessId) {
+                this.log.warn(`Skipping invalid device configuration: ${JSON.stringify(devConf)}`);
+                continue;
+            }
 
-			// get name of rodret object
-			const name = await this._getNameOfObject(devConf.rodretId);
-			const device = new RodretDevice(name, this, devConf);
-			this.devices.push(device);
-			await device.subscribe();
+            // get name of rodret object
+            const name = await this._getNameOfObject(devConf.rodretId);
+            const device = new RodretDevice(name, this, devConf);
+            this.devices.push(device);
+            await device.subscribe();
 
-			this.log.info(
-				`Configured device: rodret=${devConf.rodretId}, light=${devConf.lightId}, brightness=${devConf.brightnessId}`,
-			);
-		}
-	}
-	/**
-	 * Is called when adapter shuts down - callback has to be called under any circumstances!
-	 * @param {() => void} callback
-	 */
-	onUnload(callback) {
-		this._logverbose('Unloading adapter...');
-		try {
-			for (const dev of this.devices) {
-				dev._clearDimInterval();
-			}
+            this.log.info(
+                `Configured device: rodret=${devConf.rodretId}, light=${devConf.lightId}, brightness=${devConf.brightnessId}`,
+            );
+        }
+    }
 
-			callback();
-		} catch (_e) {
-			callback();
-		}
-	}
+    /**
+     * Is called when adapter shuts down - callback has to be called under any circumstances!
+     *
+     * @param {() => void} callback The callback function has to be called under any circumstances.
+     */
+    onUnload(callback) {
+        this._logverbose('Unloading adapter...');
+        try {
+            for (const dev of this.devices) {
+                dev._clearDimInterval();
+            }
 
-	/**
-	 * Is called if a subscribed object changes
-	 * @param {string} id
-	 * @param {ioBroker.Object | null | undefined} obj
-	 */
-	onObjectChange(id, obj) {
-		if (obj) {
-			// The object was changed
-			this._logverbose(`object ${id} changed: ${JSON.stringify(obj)}`);
-		} else {
-			// The object was deleted
-			this._logverbose(`object ${id} deleted`);
+            callback();
+        } catch (_e) {
+            callback();
+        }
+    }
 
-			this.subscribeForeignStatesAsync;
-		}
-	}
+    /**
+     * Is called if a subscribed object changes
+     *
+     * @param {string} id ID of object that changed.
+     * @param {ioBroker.Object | null | undefined} obj The object that changed (or null if it was deleted).
+     */
+    onObjectChange(id, obj) {
+        if (obj) {
+            // The object was changed
+            this._logverbose(`object ${id} changed: ${JSON.stringify(obj)}`);
+        } else {
+            // The object was deleted
+            this._logverbose(`object ${id} deleted`);
 
-	/**
-	 * Is called if a subscribed state changes
-	 * @param {string} id
-	 * @param {ioBroker.State | null | undefined} state
-	 */
-	async onStateChange(id, state) {
-		if (!state) {
-			this._logverbose(`state ${id} deleted`);
-			return;
-		}
+            this.subscribeForeignStatesAsync;
+        }
+    }
 
-		if (!state.val) {
-			return;
-		}
+    /**
+     * Is called if a subscribed state changes
+     *
+     * @param {string} id ID of state that changed.
+     * @param {ioBroker.State | null | undefined} state The state that changed (or null if it was deleted).
+     */
+    async onStateChange(id, state) {
+        if (!state) {
+            this._logverbose(`state ${id} deleted`);
+            return;
+        }
 
-		this._logverbose(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+        if (!state.val) {
+            return;
+        }
 
-		if (typeof state.val === 'string') {
-			// find according devuce instance
-			const device = this.devices.find((d) => id === d.rodretActionId);
-			if (device) {
-				await device.handleAction(state.val);
-			} else {
-				this.log.error(`Received action for unknown device: ${id}`);
-			}
-		}
-	}
+        this._logverbose(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
 
-	/**
-	 * @param {String} objectId Id of object to get the name from
-	 */
-	async _getNameOfObject(objectId) {
-		const data = await this.getForeignObjectAsync(objectId);
-		return data?.common?.name || 'UnknUnknownonw';
-	}
+        if (typeof state.val === 'string') {
+            // find according devuce instance
+            const device = this.devices.find(d => id === d.rodretActionId);
+            if (device) {
+                await device.handleAction(state.val);
+            } else {
+                this.log.error(`Received action for unknown device: ${id}`);
+            }
+        }
+    }
 
-	_logverbose(msg) {
-		if (this.config.verbose) this.log.info(msg);
-		else this.log.debug(msg);
-	}
+    /**
+     * @param {string} objectId Id of object to get the name from
+     */
+    async _getNameOfObject(objectId) {
+        const data = await this.getForeignObjectAsync(objectId);
+        return data?.common?.name || 'UnknUnknownonw';
+    }
+
+    _logverbose(msg) {
+        if (this.config.verbose) {
+            this.log.info(msg);
+        } else {
+            this.log.debug(msg);
+        }
+    }
 }
 
 if (require.main !== module) {
-	// Export the constructor in compact mode
-	/**
-	 * @param {Partial<utils.AdapterOptions>} [options={}]
-	 */
-	module.exports = (options) => new IkeaRodret(options);
+    // Export the constructor in compact mode
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options] ioBroker Adapter options.
+     */
+    module.exports = options => new IkeaRodret(options);
 } else {
-	// otherwise start the instance directly
-	new IkeaRodret();
+    // otherwise start the instance directly
+    new IkeaRodret();
 }
--- rodretdevice.js
+++ rodretdevice.js
@@ -5,139 +5,157 @@ const ACTION_BRIGHTNESS_UP = 'brightness_move_up';
 const ACTION_BRIGHTNESS_DOWN = 'brightness_move_down';
 const ACTION_BRIGHTNESS_STOP = 'brightness_stop';
 
+/**
+ * Class representing a RODRET device and its associated light.
+ */
 class RodretDevice {
-	/**
-	 * @param {String} name - Name of the rodret device
-	 * @param {IkeaRodret} adapter - Reference to the adapter
-	 * @param {object} config - RODRET device configueration
-	 */
-	constructor(name, adapter, config) {
-		this.name = name;
-		this.adapter = adapter;
-		this.config = config;
-		this.dimInterval = null;
-	}
+    /**
+     * @param {string} name - Name of the rodret device
+     * @param {IkeaRodret} adapter - Reference to the adapter
+     * @param {object} config - RODRET device configueration
+     */
+    constructor(name, adapter, config) {
+        this.name = name;
+        this.adapter = adapter;
+        this.config = config;
+        this.dimInterval = null;
+    }
 
-	/**
-	 * @returns {string} The 'action' datapoint ID of the configured RODRET device.
-	 */
-	get rodretActionId() {
-		return `${this.config.rodretId}.${ACTION_SUFFIX}`;
-	}
+    /**
+     * @returns {string} The 'action' datapoint ID of the configured RODRET device.
+     */
+    get rodretActionId() {
+        return `${this.config.rodretId}.${ACTION_SUFFIX}`;
+    }
 
-	/**
-	 * subscribe to button action event
-	 */
-	async subscribe() {
-		await this.adapter.subscribeForeignStatesAsync(this.rodretActionId);
-		this.adapter.log.info(`${this.name}: Subscribed to ${this.rodretActionId}`);
-	}
+    /**
+     * subscribe to button action event
+     */
+    async subscribe() {
+        await this.adapter.subscribeForeignStatesAsync(this.rodretActionId);
+        this.adapter.log.info(`${this.name}: Subscribed to ${this.rodretActionId}`);
+    }
 
-	/**
-	 * Handles actions triggered by the RODRET device.
-	 * @param {string} action Any of the allowed RODRET action
-	 * states ("onACTION_ON", "off", "brightness_move_up", "brightness_move_down",
-	 * "brightness_stop").
-	 */
-	async handleAction(action) {
-		if (!this._isNonEmptyString(action)) {
-			this.adapter.log.warn(`${this.name}: Ignoring empty action from RODRET device`);
-			return;
-		}
+    /**
+     * Handles actions triggered by the RODRET device.
+     *
+     * @param {string} action Any of the allowed RODRET action
+     * states ("onACTION_ON", "off", "brightness_move_up", "brightness_move_down",
+     * "brightness_stop").
+     */
+    async handleAction(action) {
+        if (!this._isNonEmptyString(action)) {
+            this.adapter.log.warn(`${this.name}: Ignoring empty action from RODRET device`);
+            return;
+        }
 
-		this._logverbose(`${this.name}: Handling action ${action} for device ${this.config.rodretId}`);
-		this._clearDimInterval();
+        this._logverbose(`${this.name}: Handling action ${action} for device ${this.config.rodretId}`);
+        this._clearDimInterval();
 
-		switch (action) {
-			case ACTION_ON:
-				await this._switchLight(true);
-				break;
+        switch (action) {
+            case ACTION_ON:
+                await this._switchLight(true);
+                break;
 
-			case ACTION_OFF:
-				await this._switchLight(false);
-				break;
+            case ACTION_OFF:
+                await this._switchLight(false);
+                break;
 
-			case ACTION_BRIGHTNESS_UP:
-				this.dimInterval = setInterval(
-					() => this._changeBrightness(this.config.dimStep),
-					this.config.dimInterval,
-				);
-				break;
+            case ACTION_BRIGHTNESS_UP:
+                this.dimInterval = setInterval(
+                    () => this._changeBrightness(this.config.dimStep),
+                    this.config.dimInterval,
+                );
+                break;
 
-			case ACTION_BRIGHTNESS_DOWN:
-				this.dimInterval = setInterval(
-					() => this._changeBrightness(-this.config.dimStep),
-					this.config.dimInterval,
-				);
-				break;
+            case ACTION_BRIGHTNESS_DOWN:
+                this.dimInterval = setInterval(
+                    () => this._changeBrightness(-this.config.dimStep),
+                    this.config.dimInterval,
+                );
+                break;
 
-			case ACTION_BRIGHTNESS_STOP:
-				this._clearDimInterval();
-				break;
+            case ACTION_BRIGHTNESS_STOP:
+                this._clearDimInterval();
+                break;
 
-			default:
-				this.adapter.log.warn(`${this.name}: Unknown action received from RODRET: ${action}`);
-				break;
-		}
-	}
+            default:
+                this.adapter.log.warn(`${this.name}: Unknown action received from RODRET: ${action}`);
+                break;
+        }
+    }
 
-	/**
-	 * Clears the dimmer interval if it exists.
-	 */
-	_clearDimInterval() {
-		if (this.dimInterval !== null) {
-			clearInterval(this.dimInterval);
-			this.dimInterval = null;
-		}
-	}
+    /**
+     * Clears the dimmer interval if it exists.
+     */
+    _clearDimInterval() {
+        if (this.dimInterval !== null) {
+            clearInterval(this.dimInterval);
+            this.dimInterval = null;
+        }
+    }
 
-	/**
-	 * @param {boolean} onOrOff
-	 */
-	async _switchLight(onOrOff) {
-		if (!this.config.lightId) {
-			// usuably should not happen...
-			this.adapter.log.error(`${this.name}: Light not configured - please check instance configuration`);
-			return;
-		}
-		await this.adapter.setForeignStateAsync(this.config.lightId, { val: onOrOff });
-	}
+    /**
+     * @param {boolean} onOrOff - True to switch on, false to switch off.
+     */
+    async _switchLight(onOrOff) {
+        if (!this.config.lightId) {
+            // usuably should not happen...
+            this.adapter.log.error(`${this.name}: Light not configured - please check instance configuration`);
+            return;
+        }
+        await this.adapter.setForeignStateAsync(this.config.lightId, {
+            val: onOrOff,
+        });
+    }
 
-	/**
-	 * Increase/decrease brightness by delta, keeping within [0, 100].
-	 * @param {number} delta - Brightness change (+/-).
-	 */
-	async _changeBrightness(delta) {
-		const current = await this.adapter.getForeignStateAsync(String(this.config.brightnessId));
-		if (!current || current.val === null) {
-			this.adapter.log.error(
-				`${this.name}: Cannot change brightness - state ${this.config.brightnessId} not found or has no value`,
-			);
-			return;
-		}
+    /**
+     * Increase/decrease brightness by delta, keeping within [0, 100].
+     *
+     * @param {number} delta - Brightness change (+/-).
+     */
+    async _changeBrightness(delta) {
+        const current = await this.adapter.getForeignStateAsync(String(this.config.brightnessId));
+        if (!current || current.val === null) {
+            this.adapter.log.error(
+                `${this.name}: Cannot change brightness - state ${this.config.brightnessId} not found or has no value`,
+            );
+            return;
+        }
 
-		const newBrightness = Math.max(0, Math.min(100, Number(current.val) + delta));
-		await this.adapter.setForeignStateAsync(String(this.config.brightnessId), { val: newBrightness });
+        const newBrightness = Math.max(0, Math.min(100, Number(current.val) + delta));
+        await this.adapter.setForeignStateAsync(String(this.config.brightnessId), {
+            val: newBrightness,
+        });
 
-		const isOn = newBrightness > 1;
-		await this._switchLight(isOn);
-	}
+        const isOn = newBrightness > 1;
+        await this._switchLight(isOn);
+    }
 
-	/**
-	 * Returns true if the input is a string with at least one
-	 * non-whitespace character.
-	 *
-	 * @param {*} value - Any value to check.
-	 * @returns {boolean}
-	 */
-	_isNonEmptyString(value) {
-		return typeof value === 'string' && value.trim().length > 0;
-	}
+    /**
+     * Returns true if the input is a string with at least one
+     * non-whitespace character.
+     *
+     * @param {*} value - Any value to check.
+     * @returns {boolean} True if value is a non-empty string.
+     */
+    _isNonEmptyString(value) {
+        return typeof value === 'string' && value.trim().length > 0;
+    }
 
-	_logverbose(msg) {
-		if (this.adapter.config.verbose) this.adapter.log.info(msg);
-		else this.adapter.log.debug(msg);
-	}
+    /**
+     * Logs a message at info level if 'verbose' config is set,
+     * otherwise at debug level.
+     *
+     * @param {string} msg The message to log.
+     */
+    _logverbose(msg) {
+        if (this.adapter.config.verbose) {
+            this.adapter.log.info(msg);
+        } else {
+            this.adapter.log.debug(msg);
+        }
+    }
 }
 
 module.exports = RodretDevice;
